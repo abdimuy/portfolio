d76685be0990f73e701b148ec6d08f37c0d52698
Aldrich <abdimuy@gmail.com>
2022-07-21 11:09:42 -0500

    [add] Agregando enlaces a la sección Work

--- src/store/works.ts
+++ src/store/works.ts
@@ -5,14 +5,14 @@ export const works = [
     title: "Portfolio",
     description: "A modern UI/UX Portfolio Website",
     imageUrl: images.portfolio,
-    websiteURL: "",
+    websiteURL: "https://abdimuy.github.io",
     githubUrl: "https://github.com/abdimuy/portfolio",
   },
   {
     title: "NFT Landing Page",
     description: "A Modern NFT Landing Page",
     imageUrl: images.landingPage,
-    websiteURL: "",
+    websiteURL: "https://nftlandingpageproj.netlify.app/",
     githubUrl: "https://github.com/abdimuy/nftlandingpage",
   },
 ];
